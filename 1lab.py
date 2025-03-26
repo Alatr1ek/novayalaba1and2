f60e0d61a68a2e01c14b02f72919e0b6cd35f574
Alatr1ek <63065955+Alatr1ek@users.noreply.github.com>
2025-03-27 01:25:00 +0400

    Add files via upload

--- 1lab.py
+++ 1lab.py
@@ -1,18 +1,15 @@
+import re
+
 def to_words(n):
-    d = "ноль один два три четыре пять шесть семь восемь девять".split()
-    return " ".join(d[int(c)] for c in str(n))
+    return " ".join("ноль один два три четыре пять шесть семь восемь девять".split()[int(c)] for c in str(n))
 
-min_n = float('inf')
-max_n = float('-inf')
+min_n, max_n = float('inf'), float('-inf')
 
-with open("1lab.txt", "r") as file:
-    for line in file:
-        for token in line.split():
-            if all(c in "01234567" for c in token):
-                n = int(token, 8)
-                if n <= 1023 and len(token) > 1 and token[-2] == '1':
-                    print(token[:-2])
-                    min_n, max_n = min(min_n, n), max(max_n, n)
+with open("1lab.txt") as f:
+    for token in re.findall(r'\b[0-7]+\b', f.read()):
+        if len(token) > 1 and token[-2] == '1' and (n := int(token, 8)) <= 1023:
+            print(token[:-2])
+            min_n, max_n = min(min_n, n), max(max_n, n)
 
 if min_n != float('inf'):
     print(to_words((min_n + max_n) // 2))

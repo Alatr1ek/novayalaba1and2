b67894eb5a74e02f976de4f3c6e63a511ec6ea57
Alatr1ek <63065955+Alatr1ek@users.noreply.github.com>
2025-10-15 19:58:53 +0400

    Update 2laba.py

--- 2lab/2laba.py
+++ 2lab/2laba.py
@@ -1,14 +1,14 @@
 import re
 
+
 def to_words(n):
     words = "ноль один два три четыре пять шесть семь восемь девять".split()
     return " ".join(words[int(c)] for c in str(n))
 
 with open("1lab.txt") as f:
     content = f.read()
-    m = re.findall(r'\b[0-7]{0,2}*1[0-7]\b', content)
-    
-if m:
-    filter1 = filter(lambda a: int(a, 8) <= 1023, m)
-    print("\n".join(map(lambda a: a[:-2], filter1)))
-    print(to_words((min(nums) + max(nums)) // 2))
+    m = re.findall(r'\b([0-1]{0,2}1[0-7]|[0-7]{0,1}1[0-7])\b', content)
+    nums = list(map(lambda x: int(x, 8), m))
+    print("\n".join(map(lambda a: a[:-2], m)))
+    if nums:
+        print(to_words((min(nums) + max(nums)) // 2))
